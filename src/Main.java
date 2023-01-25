e635db60726f4db8edaf5e2f83646570eabf34b3
robwidel <robwidel@gmail.com>
2023-01-25 17:19:51 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Give me an INT number: ");
+        String str = scan.nextLine();
+        Scanner scanner = new Scanner(str);
+        int x;
+
+        try {
+            if(scanner.hasNextInt()){
+                x = scanner.nextInt();
+                System.out.println("Takie dales int: " + x);
+            }
+        } catch(Exception e) {
+            System.out.println("Something went wrong.");
+        } finally {
+            System.out.println("The 'try catch' is finished.");
+        }
+
+
+    }
+
+
+}
